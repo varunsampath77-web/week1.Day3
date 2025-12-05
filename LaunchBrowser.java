754c7ff05aa76d5b18a877e3ff7acb6a6965406e
Hemvarun <varunsampath77@gmail.com>
2025-12-05 12:59:32 +0530

    Add files via upload
    BrowserLaunch

---
+++ LaunchBrowser.java
@@ -0,0 +1,13 @@
+package day3;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.get("https://www.proton.com/");
+
+	}
+
+}
