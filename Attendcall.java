ce529bb8276d9a51c9acdfbc66396947ed369e3e
Hemvarun <varunsampath77@gmail.com>
2025-12-05 11:43:03 +0530

    Add files via upload
    Attend call

---
+++ Attendcall.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Attendcall {
+	public void attendcall(float number) {
+		System.out.println(number);
+	}
+
+	public static void main(String[] args) {
+		Attendcall obj = new Attendcall();
+		obj.attendcall(954662781);
+		// TODO Auto-generated method stub
+	}
+
+}
