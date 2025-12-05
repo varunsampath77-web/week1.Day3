132b3b8c2002c5cc36665e8d2fd9974a9d7b5e96
Hemvarun <varunsampath77@gmail.com>
2025-12-05 10:18:39 +0530

    Add files via upload
    Methods

---
+++ Mobile.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Mobile {
+	public void makecall() {
+		System.out.println("make call");
+	}
+
+	public static void main(String[] args) {
+		Mobile obj = new Mobile();
+		obj.makecall();
+	
+	}
+
+}
