d38a844b6e83a7629757e68f877535de8c83614d
Hemvarun <varunsampath77@gmail.com>
2025-12-05 15:04:56 +0530

    Add files via upload
    Facebook login

--- LaunchBrowser.java
+++ LaunchBrowser.java
@@ -1,13 +1,27 @@
 package day3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class LaunchBrowser {
 
 	public static void main(String[] args) {
-		 ChromeDriver driver=new ChromeDriver();
-		 driver.get("https://www.proton.com/");
-
+		 //Launch chrome browser
+		//ChromeDriver driver=new ChromeDriver();
+		
+		EdgeDriver driver=new EdgeDriver();
+		 
+		//load url
+		driver.get("https://www.facebook.com/");
+		
+		
+		driver.findElement(By.id("email")).sendKeys("8925411170");
+		
+		driver.findElement(By.id("pass")).sendKeys("Prathap");
+		
+		driver.findElement(By.name("login")).click();
+		
 	}
-
 }
+
